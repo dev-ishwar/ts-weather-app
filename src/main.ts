ac73dbde5d4892d1886a78704d4622bfedd27b3e
Ishwar Singh <ishwar0647@gmail.com>
2025-01-27 17:31:22 +0530

    Implement notification

--- src/main.ts
+++ src/main.ts
@@ -4,11 +4,13 @@ import SearchList from "./model/SearchList";
 import { SearchItem } from "./model/SearchListItem";
 import CurrentWeatherTemplate from "./template/CurrentWeatherTemplate";
 import { Loader } from "./template/Loader";
+import { NofificationTypeEnum, NotificationTemplate } from "./template/NotificationTemplate";
 import SearchTemplate from "./template/SearchTemplate";
 
 const searchList = SearchList.instance;
 const searchTemplace = SearchTemplate.instance;
 const loader = Loader.instance;
+const notification = NotificationTemplate.instance;
 
 const searchLocation = async (query: string) => {
   if (!query) {
@@ -28,10 +30,18 @@ export const handleLocationClick = async (place: SearchItem) => {
   loader.show();
 
   const template = CurrentWeatherTemplate.instance;
-  const res = await fetchCurrentWeather(place.county ?? place.city);
+  const q = `${place.lat},${place.lon}`; //  place.city ?? place.county;
+  const res = await fetchCurrentWeather(q);
 
   console.log('error: ', res?.error);
+
+  if (res?.error) {
+    notification.show(res.error, NofificationTypeEnum.ERROR);
+  }
+
   if (res?.weather) {
+    notification.show(`Weather for ${place.formatted}`);
+  
     searchTemplace.clear();
     template.render(res.weather);
   }
---
+++ src/template/NotificationTemplate.ts
@@ -0,0 +1,63 @@
+
+interface INofication {
+    show(msg: string, type?: string): void;
+    hide(): void;
+    showBar(): void;
+}
+
+export enum NofificationTypeEnum {
+    ERROR = "ERROR",
+    SUCCESS = 'SUCCESS',
+}
+
+export class NotificationTemplate implements INofication {
+    static readonly instance = new NotificationTemplate();
+    container: HTMLDivElement;
+    message: HTMLParagraphElement;
+    bar: HTMLDivElement;
+    timeout?: number;
+    private constructor() {
+        this.container = document.getElementById('notification') as HTMLDivElement;
+        this.message = document.getElementById('notification__message') as HTMLParagraphElement;
+        this.bar = document.querySelector('.notification__bar') as HTMLDivElement;
+
+        const button = document.getElementById('close-notification') as HTMLButtonElement;
+
+        button.addEventListener('click', () => {
+            this.hide();
+        })
+    }
+
+    showBar(): void {
+        if (this.timeout) clearInterval(this.timeout);
+        let w = 100;
+        this.timeout = setInterval(() => {
+            if (w <= 0) {
+                clearInterval(this.timeout);
+                this.hide();
+            }
+            w -= 1;
+            if (this.bar.style) this.bar.style.width = `${w}%`;
+        }, 50)
+    }
+
+
+    show(msg: string, type?: string): void {
+        this.message.innerText = msg;
+
+        if (type === NofificationTypeEnum.ERROR) {
+            this.container.style.color = 'red';
+        }
+
+        this.container.classList.remove('hide');
+        this.container.classList.add('show');
+        console.log('bar ', this.bar)
+        this.showBar();
+    }
+
+    hide(): void {
+        this.container.classList.add('hide');
+        this.container.classList.remove('show');
+
+    }
+}
